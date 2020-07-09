88dc411c20b7cd1049bc06f8feb11d4853166146
imAdarshh <adarshchoudhary087@gmail.com>
2020-07-09 14:53:23 +0530

    Unification

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $("#myCarousel").carousel({interval:2000});
+  $("#carouselButton").click(function(){
+    if ($("#carouselButton").children('span').hasClass("fa-pause")){
+      $("#myCarousel").carousel('pause');
+      $("#carouselButton").children('span').removeClass("fa-pause");
+      $("#carouselButton").children('span').addClass("fa-play");
+    }
+    else {
+      $("#myCarousel").carousel('cycle');
+      $("#carouselButton").children('span').removeClass("fa-play");
+      $("#carouselButton").children('span').addClass("fa-pause");
+    }
+  });
+});
+
+$(document).ready(function(){
+  $("#reserveButton").click(function(){
+    $("#formModal").modal("show");
+  });
+  $("#loginButton").click(function(){
+    $("#loginModal").modal("show");
+  });
+});
